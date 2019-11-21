de9ed9d6da01cfcbda40bbb234f5dac26c6b73f6
brunocheble <contato.cheble@gmail.com>
2019-11-21 12:27:51 +0000

    Change parameter!

--- src/app/controllers/RequesterController.js
+++ src/app/controllers/RequesterController.js
@@ -4,13 +4,13 @@ const axios = require('axios');
 class RequesterController {
     async first(req, res) {
         const { _groupId } = req.params;
-        const bot = await Bot.findOneAndUpdate({ _groupId, active: true }, {}).sort([['updatedAt', 1]]);        
+        const bot = await Bot.findOneAndUpdate({ _groupId, active: true }, {}).sort([['updatedAt', 1]]);
         const { type, url } = bot._doc;
-
+        
         axios({
             method: type,
             url,
-            data: req.body
+            data: req.headers
         }).then(function (response) {
             console.log(response);
         });
