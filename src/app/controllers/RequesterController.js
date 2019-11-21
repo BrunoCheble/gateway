a5dca7fe97ebd517d51735d4cad168632dbde69f
brunocheble <contato.cheble@gmail.com>
2019-11-21 12:08:54 +0000

    Fixed parameters

--- src/app/controllers/RequesterController.js
+++ src/app/controllers/RequesterController.js
@@ -4,17 +4,18 @@ const axios = require('axios');
 class RequesterController {
     async first(req, res) {
         const { _groupId } = req.params;
-        const bot = await Bot.findOneAndUpdate({ _groupId, active: true }, {}).sort([['updatedAt', 1]]);
+        const bot = await Bot.findOneAndUpdate({ _groupId, active: true }, {}).sort([['updatedAt', 1]]);        
+        const { type, url } = bot._doc;
 
         axios({
-            method: bot.type,
-            url: bot.url,
+            method: type,
+            url,
             data: req.body
         }).then(function (response) {
             console.log(response);
         });
 
-        return res.json({ ...bot, data: req.body });
+        return res.json({ type, url, data: req.body });
     }
 }
 
