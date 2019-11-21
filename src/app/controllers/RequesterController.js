f7f91228eba71aa1503c6830bf8d6f9e38b137d7
brunocheble <contato.cheble@gmail.com>
2019-11-21 11:58:33 +0000

    Development of the request bot;

--- src/app/controllers/RequesterController.js
+++ src/app/controllers/RequesterController.js
@@ -1,10 +1,20 @@
 const Bot = require('../models/Bot');
+const axios = require('axios');
 
 class RequesterController {
     async first(req, res) {
         const { _groupId } = req.params;
-        const bot = await Bot.findOneAndUpdate({ _groupId, active: true },{}).sort([['updatedAt', 1]]);
-        return res.json({ bot });
+        const bot = await Bot.findOneAndUpdate({ _groupId, active: true }, {}).sort([['updatedAt', 1]]);
+
+        axios({
+            method: bot.type,
+            url: bot.url,
+            data: req.body
+        }).then(function (response) {
+            console.log(response);
+        });
+
+        return res.json({ ...bot, data: req.body });
     }
 }
 
